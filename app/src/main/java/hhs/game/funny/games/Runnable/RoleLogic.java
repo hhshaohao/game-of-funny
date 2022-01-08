6a734a22fd61021f23369f8817b39c4ab11dc34b
hhshaohao <hejunhao2468@163.com>
2022-01-08 20:04:39 +0800

    造轮子ing

---
+++ app/src/main/java/hhs/game/funny/games/Runnable/RoleLogic.java
@@ -0,0 +1,10 @@
+package hhs.game.funny.games.Runnable;
+
+public abstract class RoleLogic {
+	//左移动按钮被点击
+    public abstract void leftAction();
+    //右移动按钮被点击
+	public abstract void rightAction();
+	//跳跃按钮被点击
+	public abstract void upAction();
+}
--- app/src/main/java/hhs/game/funny/games/Stage/UserInterface.java
+++ app/src/main/java/hhs/game/funny/games/Stage/UserInterface.java
@@ -1,12 +1,68 @@
 package hhs.game.funny.games.Stage;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import hhs.game.funny.games.MyGame;
+import hhs.game.funny.games.Res;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import hhs.game.funny.games.Runnable.RoleLogic;
 
-public class UserInterface extends Stage {
-    
-	public UserInterface()
+public class UserInterface extends Stage
+{
+
+	MyGame game;
+
+	ImageButton b0,b1,b2;
+	RoleLogic logic;
+
+	public UserInterface(final MyGame game,RoleLogic l)
 	{
+		this.game = game;
+
+		Res r = new Res(game);
+		b0 = r.b0;
+		b1 = r.b1;
+		b2 = r.b2;
 		
+		this.logic = l;
+		this.addButtonAction();
+
+		this.addActor(r.exit);
+		this.addActor(b0);
+		this.addActor(b1);
+		this.addActor(b2);
+	}
+
+	private void addButtonAction()
+	{
+		b0.addListener(new InputListener()
+			{
+				@Override
+				public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+				{
+					logic.leftAction();
+					return true;
+				}
+			});
+		b1.addListener(new InputListener()
+			{
+				@Override
+				public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+				{
+					logic.rightAction();
+					return true;
+				}
+			});
+		b2.addListener(new InputListener()
+			{
+				@Override
+				public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+				{
+					logic.upAction();
+					return true;
+				}
+			});
 	}
 
 	@Override
@@ -20,5 +76,5 @@ public class UserInterface extends Stage {
 	{
 		super.draw();
 	}
-    
+
 }
