71c391930790e432af8826c9e244892875b1c69f
hhshaohao <hejunhao2468@163.com>
2022-01-11 18:08:21 +0800

    造轮子

---
+++ app/src/main/java/hhs/game/funny/games/Actor/FastRoleActor.java
@@ -0,0 +1,61 @@
+package hhs.game.funny.games.Actor;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import hhs.game.funny.games.Tools.Controler;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class FastRoleActor extends Actor{
+    
+    World world;
+	Body b2body;
+	Texture texture;
+	float sx,sy,radius;	//self X,self Y
+	Controler con;
+	
+	public FastRoleActor(World w,Texture text,int radius,Vector2 ve)
+	{
+		this.world = w;
+		this.texture = text;
+		this.radius = radius;
+		
+		BodyDef bdef = new BodyDef();
+		bdef.type = BodyDef.BodyType.DynamicBody;
+		
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(radius,radius);
+		
+		FixtureDef fdef = new FixtureDef();
+		fdef.shape  = shape;
+		
+		bdef.position.set(ve);
+		b2body = world.createBody(bdef);
+		b2body.createFixture(fdef);
+	}
+
+	@Override
+	public void act(float delta)
+	{
+		sx = b2body.getPosition().x - radius;
+		sy = b2body.getPosition().y - radius;
+		con.frameCall(b2body);
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha)
+	{
+		batch.draw(texture,sx,sy,radius,radius);
+	}
+    
+	public void addControler(Controler con)
+	{
+		this.con = con;
+	}
+	
+}
